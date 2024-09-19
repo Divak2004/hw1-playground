a583e53cc3831e9d3ffbf2b7a9a826f1dbed65d9
Divak2004 <fu_david@outlook.com>
2024-09-18 22:16:41 -0400

    Update README and fixed some bugs

--- README.md
+++ README.md
@@ -1,49 +1,47 @@
-# *(game title)*
-
-*For instructions, consult the [CIS 1951 website](https://www.seas.upenn.edu/~cis1951/24fa/assignments/hw/hw1).*
+# *Terra Exploration*
 
 ## Explanations
 
 **What locations/rooms does your game have?**
 
-1. 
-2. 
-3. 
-4. 
-5. 
-6. 
-7. 
+1. Plains
+2. Victoria
+3. Leithania
+4. Kazdel
+5. Unknown Area
+6. Cave
+7. Yan
 8. 
 9. 
 10. 
 
 **What items does your game have?**
 
-1. 
+1. Black ring (can be obtained by interacting with Amiya and 'aid' her)
 2. 
 
 **Explain how your code is designed. In particular, describe how you used structs or enums, as well as protocols.**
 
-*(insert a brief explanation)*
+*I designed a protocol called GameObject which my structs, Location and Character, used. The GameObject protocol requires anything interactable to have a name, a description, and commands. It also forces anything implementing the protocol to have a way of displaying their "content", aka their name and description, as well as the commands that a user can use to interact with the object. Besides the variables and functions required in the GameObject protocol, the Location struct also contains information on other locations that it is connected to as well as a character associated to it, with the character potentially being nil. The Character struct also contains a variable storing the 'reactions' that the character can have depending on the user's commands.*
 
 **How do you use optionals in your program?**
 
-*(insert a brief explanation)*
+*I used optionals nontrivially to allow a location to have the option to store nil as the character associated to it. Hence, when I check to see if I should display a character's information, one of the conditions that I check is if the character is nil.*
 
 **What extra credit features did you implement, if any?**
 
-* *(if no extra credit, write "None")*
+* *I added rich text by changing the color of game over depending on how the game ended. If the player dies, it is red, and if the player survives till the end, it is green.*
 
 ## Endings
 
 ### Ending 1
 
 ```
-[insert series of commands to reach ending 1]
+east ask north west leave west south
 ```
 
 ### Ending 2
 
 ```
-[insert series of commands to reach ending 2]
+east leave north west leave
 ```
--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -37,10 +37,10 @@ struct Location: GameObject {
     var name: String
     var description: String
     var commands: String
-    var otherLocations: Array<Location>
+    var otherLocations: Array<Location?>
     var char: Char?
     
-    init (n: String, desc: String, coms: String, locations: Array<Location>, charac: Char?) {
+    init (n: String, desc: String, coms: String, locations: Array<Location?>, charac: Char?) {
         name = n
         description = desc
         commands = coms
@@ -57,23 +57,23 @@ struct Location: GameObject {
     }
 }
 
-let amiya: Char = Char(n: "Amiya", desc: "A cautus, which is one of the ancient races on Terra. She has black rings that seem to suppress her power. Perhaps she may help you if you ask?", coms: "help, ask, ignore", reacts: ("She hands you one of her rings. As you leave, she says: 'Go North, stranger. May your journey be safe.'", "You move on in peace."))
-let talulah: Char = Char(n: "Talulah", desc: "A draco, which is another one of the ancient races on Terra. She has a long sword by her waist. She is injured and lying down, but something seems suspicious.", coms: "help, aid, leave", reacts: ("As you approach her, she suddenly gets up and swings her sword, now in flames, at you. You couldn't dodge in time and you die.", "As you leave, she suddenly gets up and starts chasing you. You began running away as fast as possible. As you run, you see a sign that says the following: west - cave."))
-
-let yan: Location = Location(n: "Yan", desc: "A prosperous nation with a stable government and a strong economy. While you still have no idea where your family is, you breathe a sigh of relief that you are not chased anymore. You decide that this is a good place to settle for now before proceeding to search for your family.", coms: "", locations: [], charac: nil)
-let cave: Location = Location(n: "Cave", desc: "A cave whose depth is unknown. As you run for who knows how long, you finally arrive at a sharp turn. Lo and behold, you see a ray of light from the South.", coms: "help, south", locations: [yan], charac: nil)
-let deadEnd: Location = Location(n: "Unknown Area", desc: "A misty area engulfed in darkness. You wander around for days on end, but to no avail. In the end, you manage to escape. How, you may ask. Through death.", coms: "", locations: [], charac: nil)
-let kazdel: Location = Location(n: "Kazdel", desc: "A wartorn nation filled with dead bodies and the stench of blood. With Talulah still chasing you, you have no choice but to move on and ignore the cries for help from the victims of war.", coms: "help, west, north, south", locations: [cave, deadEnd, deadEnd], charac: nil)
-let leithania: Location = Location(n: "Leithania", desc: "A small nation filled with nobles that has managed to weaponized music. You managed to successfully enter the nation, and you managed to find out that people who looked like you went west.", coms: "help, west", locations: [kazdel], charac: talulah)
-let victoria: Location = Location(n: "Victoria", desc: "A glorious nation with steam knights defending it. However, those knights are nowhere to be seen. You feel a dangerous aura around, so you hurry and try to leave.", coms: "help, north, east, west", locations: [leithania, deadEnd, deadEnd], charac: nil)
-let beginning: Location = Location(n: "Plains", desc: "A plain field with nothing but grass. While there are no directions around, your gut tells you to go east. Perhaps you can find more information about your family there.", coms: "help, east", locations: [victoria], charac: amiya)
-
 /// Declare your game's behavior and state in this struct.
 ///
 /// This struct will be re-created when the game resets. All game state should
 /// be stored in this struct.
 struct YourGame: AdventureGame {
-    private var curLocation: Location = beginning
+    private let amiya: Char = Char(n: "Amiya", desc: "A cautus, which is one of the ancient races on Terra. She has black rings that seem to suppress her power. Perhaps she may help you if you ask?", coms: "help, ask, ignore", reacts: ("She hands you one of her rings. As you leave, she says: 'Go North, stranger. May your journey be safe.'", "You move on in peace."))
+    private let talulah: Char = Char(n: "Talulah", desc: "A draco, which is another one of the ancient races on Terra. She has a long sword by her waist. She is injured and lying down, but something seems suspicious.", coms: "help, aid, leave", reacts: ("As you approach her, she suddenly gets up and swings her sword, now in flames, at you. You couldn't dodge in time and you die.", "As you leave, she suddenly gets up and starts chasing you. You began running away as fast as possible. As you run, you see a sign that says the following: west - cave."))
+
+    private var yan: Location? = nil
+    private var cave: Location? = nil
+    private var deadEnd: Location? = nil
+    private var kazdel: Location? = nil
+    private var leithania: Location? = nil
+    private var victoria: Location? = nil
+    private var beginning: Location? = nil
+    
+    private var curLocation: Location? = nil
     private var curChar: Char? = nil
     private var picked: Bool = false
     private var displayedChar: Bool = false
@@ -93,9 +93,18 @@ struct YourGame: AdventureGame {
     ///
     /// - Parameter context: The object you use to write output and end the game.
     mutating func start(context: AdventureGameContext) {
+        yan = Location(n: "Yan", desc: "A prosperous nation with a stable government and a strong economy. While you still have no idea where your family is, you breathe a sigh of relief that you are not chased anymore. You decide that this is a good place to settle for now before proceeding to search for your family.", coms: "", locations: [], charac: nil)
+        cave = Location(n: "Cave", desc: "A cave whose depth is unknown. As you run for who knows how long, you finally arrive at a sharp turn. Lo and behold, you see a ray of light from the South.", coms: "help, south", locations: [yan], charac: nil)
+        deadEnd = Location(n: "Unknown Area", desc: "A misty area engulfed in darkness. You wander around for days on end, but to no avail. In the end, you manage to escape. How, you may ask. Through death.", coms: "", locations: [], charac: nil)
+        kazdel = Location(n: "Kazdel", desc: "A wartorn nation filled with dead bodies and the stench of blood. With Talulah still chasing you, you have no choice but to move on and ignore the cries for help from the victims of war.", coms: "help, west, north, south", locations: [cave, deadEnd, deadEnd], charac: nil)
+        leithania = Location(n: "Leithania", desc: "A small nation filled with nobles that has managed to weaponized music. You managed to successfully enter the nation, and you managed to find out that people who looked like you went west.", coms: "help, west", locations: [kazdel], charac: talulah)
+        victoria = Location(n: "Victoria", desc: "A glorious nation with steam knights defending it. However, those knights are nowhere to be seen. You feel a dangerous aura around, so you hurry and try to leave.", coms: "help, north, east, west", locations: [leithania, deadEnd, deadEnd], charac: nil)
+        beginning = Location(n: "Plains", desc: "A plain field with nothing but grass. While there are no directions around, your gut tells you to go east. Perhaps you can find more information about your family there.", coms: "help, east", locations: [victoria], charac: amiya)
+        
+        
         context.write("Welcome to " + title + "!")
         curLocation = beginning
-        context.write(curLocation.displayContent())
+        context.write(curLocation!.displayContent())
         displayedLoc = true
     }
     
@@ -132,28 +141,37 @@ struct YourGame: AdventureGame {
         
         let arg = arguments[0]
         
+        // Only runs if the character's description is rendered
         if displayedChar {
             if arg == "help" {
                 context.write(curChar!.displayCommands())
                 return
             } else {
                 let commands = curChar!.commands.split(separator: ", ")
+                // Cases based on char interaction
                 switch arg {
                     case commands[1]:
                         context.write(curChar!.reactions.0)
                         if (curChar!.name == "Amiya") {
                             picked = true
                         } else {
+                            var attributedString = AttributedString("Game Over")
+                            attributedString.swiftUI.foregroundColor = .red
+                            context.write(attributedString)
                             context.endGame()
                             return
                         }
                     case commands[2]:
                         context.write(curChar!.reactions.1)
                         if (curChar!.name == "Talulah") {
+                            // Case where item picked from Amiya affects game and doesn't end game.
                             if picked {
                                 context.write("Before you realize it, Talulah was at your heels! Thankfully, the ring you received from Amiya started activating, unleashing some mystical black magic onto Talulah and causing her to fall onto the ground. You take this chance to quickly run away while thinking about where you've seen this before.")
                             } else {
                                 context.write("Before you realize it, Talulah was at your heels! You try to put more distance between yourself and Talulah, but she keeps on getting closer. Eventually, you run out of energy and collapse while leaving yourself at the mercy of her flaming sword.")
+                                var attributedString = AttributedString("Game Over")
+                                attributedString.swiftUI.foregroundColor = .red
+                                context.write(attributedString)
                                 context.endGame()
                                 return
                             }
@@ -168,16 +186,21 @@ struct YourGame: AdventureGame {
             displayedChar = false
             curChar = nil
         }
+        // Checks to see if location description is already shown
         if !displayedLoc {
             displayedLoc = true
         }
-        let commands = curLocation.commands.split(separator: ", ")
-        let locations = curLocation.otherLocations
+        let commands = curLocation!.commands.split(separator: ", ")
+        let locations = curLocation!.otherLocations
+        
+        // Cases depending on user input
         if arg == "help" {
-            context.write(curLocation.displayCommands())
-        } else if commands.count > 1 && (arg == commands[1] || locationCommand == commands[1]) {
+            context.write(curLocation!.displayCommands())
+        } 
+        // Case to determine whether to display character or not depending on whether location has it or not
+        else if commands.count > 1 && (arg == commands[1] || locationCommand == commands[1]) {
             if locationCommand == "" {
-                curChar = curLocation.char
+                curChar = curLocation!.char
             }
             if curChar != nil && !displayedChar {
                 context.write(curChar!.displayContent())
@@ -185,24 +208,24 @@ struct YourGame: AdventureGame {
                 locationCommand = String(arg)
             } else {
                 curLocation = locations[0]
-                context.write(curLocation.displayContent())
+                context.write(curLocation!.displayContent())
                 locationCommand = ""
             }
             displayedLoc = false
         } else if commands.count > 2 && arg == commands[2] {
             curLocation = locations[1]
-            context.write(curLocation.displayContent())
+            context.write(curLocation!.displayContent())
             displayedLoc = false
         } else if commands.count > 3 && arg == commands[3] {
             curLocation = locations[2]
-            context.write(curLocation.displayContent())
+            context.write(curLocation!.displayContent())
             displayedLoc = false
         } else {
             context.write("Invalid command")
         }
-        if curLocation.commands == "" {
+        if curLocation!.commands == "" {
             var attributedString = AttributedString("Game Over")
-            if curLocation.name == "Yan" {
+            if curLocation!.name == "Yan" {
                 attributedString.swiftUI.foregroundColor = .green
             } else {
                 attributedString.swiftUI.foregroundColor = .red
